74eb9fe8d5d93460800142b9241d8e987f0ba55c
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2024-08-23 19:35:11 -0700

    Complete lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,10 @@
-def write_file(file_name, file_content):
-    pass
+def write_file(file_name="lib/test_file", file_content="This is a test content."):
+    with open(f'{file_name}.txt', mode="w", encoding="utf-8") as test_file:
+        test_file.write(file_content)
 
-def append_file(file_name, append_content):
-    pass
+def append_file(file_name="lib/test_file", append_content="This is a test content."):
+    with open(f'{file_name}.txt', mode="a", encoding="utf-8") as test_file:
+        test_file.write(append_content)
 
-def read_file(file_name):
-    pass
+def read_file(file_name='test_file'):
+    return open(f'{file_name}.txt', encoding='utf-8').read()
